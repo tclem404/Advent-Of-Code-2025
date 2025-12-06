b02fa8454d329ae2000ac3caa8885f5298758cac
Ty Clements <tclem404@gmail.com>
2025-12-05 21:55:06 -0800

    comments

--- Day6/problem2.py
+++ Day6/problem2.py
@@ -9,15 +9,16 @@ total = 0
 for i in range(len(operation)):
     prod = 1
 
+    # get matrix of numbers
     inputs = [list(line[operation[i][0]: operation[i + 1][0] - 1 if i + 1 < len(operation) else -1]) for line in lines[:-1]]
 
-    # transpose
+    # transpose list arr
     inputs = [list(row) for row in zip(*inputs)]
 
+    # make numbers
     inputs = [int(''.join(num)) for num in inputs]
 
-    print(inputs)
-
+    # do operation
     for j in range(len(inputs)):
         if operation[i][1] == '+':
             total += inputs[j]
